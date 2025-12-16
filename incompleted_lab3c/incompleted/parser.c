fc95ea6de2acacee3872c870b02bc4403f43306d
NhMinh-2610 <nnhatminh2610@gmail.com>
2025-12-16 08:18:52 +0700

    Fix

--- incompleted_lab3c/incompleted/parser.c
+++ incompleted_lab3c/incompleted/parser.c
@@ -59,7 +59,8 @@ void compileBlock(void) {
 
     do {
       eat(TK_IDENT);
-      // TODO: Check if a constant identifier is fresh in the block
+      // Kiểm tra tên hằng có hợp lệ không
+      checkFreshIdent(currentToken->string);
 
       // Create a constant object
       constObj = createConstantObject(currentToken->string);
@@ -88,7 +89,8 @@ void compileBlock2(void) {
 
     do {
       eat(TK_IDENT);
-      // TODO: Check if a type identifier is fresh in the block
+      // Kiểm tra tên kiểu có hợp lệ không
+      checkFreshIdent(currentToken->string);
 
       // create a type object
       typeObj = createTypeObject(currentToken->string);
@@ -117,7 +119,8 @@ void compileBlock3(void) {
 
     do {
       eat(TK_IDENT);
-      // TODO: Check if a variable identifier is fresh in the block
+      // Kiểm tra tên biến có hợp lệ không
+      checkFreshIdent(currentToken->string);
 
       // Create a variable object      
       varObj = createVariableObject(currentToken->string);
@@ -162,7 +165,8 @@ void compileFuncDecl(void) {
 
   eat(KW_FUNCTION);
   eat(TK_IDENT);
-  // TODO: Check if a function identifier is fresh in the block
+  // Kiểm tra tên hàm có hợp lệ không
+  checkFreshIdent(currentToken->string);
 
   // create the function object
   funcObj = createFunctionObject(currentToken->string);
@@ -189,7 +193,7 @@ void compileProcDecl(void) {
 
   eat(KW_PROCEDURE);
   eat(TK_IDENT);
-  // TODO: Check if a procedure identifier is fresh in the block
+  // Check if a procedure identifier is fresh in the block
   checkFreshIdent(currentToken->string);
   // create a procedure object
   procObj = createProcedureObject(currentToken->string);
@@ -218,8 +222,9 @@ ConstantValue* compileUnsignedConstant(void) {
     break;
   case TK_IDENT:
     eat(TK_IDENT);
-    // TODO: check if the constant identifier is declared and get its value
-
+    // Kiểm tra hằng số đã khai báo và lấy giá trị
+    obj = checkDeclaredConstant(currentToken->string);
+    constValue = duplicateConstantValue(obj->constAttrs->value);
     break;
   case TK_CHAR:
     eat(TK_CHAR);
@@ -267,7 +272,12 @@ ConstantValue* compileConstant2(void) {
     break;
   case TK_IDENT:
     eat(TK_IDENT);
-    // TODO: check if the integer constant identifier is declared and get its value
+        // Kiểm tra hằng số nguyên đã khai báo và lấy giá trị
+    obj = checkDeclaredConstant(currentToken->string);
+    if (obj->constAttrs->value->type != TP_INT) {
+      error(ERR_UNDECLARED_INT_CONSTANT, currentToken->lineNo, currentToken->colNo);
+    }
+    constValue = duplicateConstantValue(obj->constAttrs->value);
     break;
   default:
     error(ERR_INVALID_CONSTANT, lookAhead->lineNo, lookAhead->colNo);
@@ -305,7 +315,9 @@ Type* compileType(void) {
     break;
   case TK_IDENT:
     eat(TK_IDENT);
-    // TODO: check if the type idntifier is declared and get its actual type
+        // Kiểm tra kiểu đã khai báo và lấy kiểu thực tế
+    obj = checkDeclaredType(currentToken->string);
+    type = duplicateType(obj->typeAttrs->actualType);
     break;
   default:
     error(ERR_INVALID_TYPE, lookAhead->lineNo, lookAhead->colNo);
@@ -364,7 +376,8 @@ void compileParam(void) {
   }
 
   eat(TK_IDENT);
-  // TODO: check if the parameter identifier is fresh in the block
+   // Kiểm tra tên tham số có hợp lệ không
+  checkFreshIdent(currentToken->string);
   param = createParameterObject(currentToken->string, paramKind, symtab->currentScope->owner);
   eat(SB_COLON);
   type = compileBasicType();
@@ -429,9 +442,11 @@ void compileAssignSt(void) {
 }
 
 void compileCallSt(void) {
+  Object *proc;
   eat(KW_CALL);
   eat(TK_IDENT);
-  // TODO: check if the identifier is a declared procedure
+    // Kiểm tra thủ tục đã khai báo
+  proc = checkDeclaredProcedure(currentToken->string);
   compileArguments();
 }
 
@@ -463,10 +478,12 @@ void compileWhileSt(void) {
 }
 
 void compileForSt(void) {
+  Object *var;
   eat(KW_FOR);
   eat(TK_IDENT);
 
-  // TODO: check if the identifier is a variable
+  // Kiểm tra định danh là biến
+  var = checkDeclaredVariable(currentToken->string);
 
   eat(SB_ASSIGN);
   compileExpression();
